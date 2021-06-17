a35ea170b930fe6e8c8bb4bf10e2373525bbdebd
Alejandro Lopez <alejandro.lopez@phila.gov>
2021-06-17 16:29:46 -0400

    fix: update rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,34 @@
 import commonjs from 'rollup-plugin-commonjs'; // Convert CommonJS modules to ES6
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 
 export default [ 'vue-sso' ].map((name) => ({
   input: `${name}.js`,
-  output: [{
-    format: 'umd',
-    name,
-    file: `dist/${name}.umd.js`,
-  },
-  {
-    format: 'es',
-    name,
-    exports: 'named',
-    file: `dist/${name}.esm.js`,
-  }, {
-    format: 'iife',
-    name,
-    extend: true,
-    exports: 'named',
-    file: `dist/${name}.min.js`,
-  },
+  output: [
+    {
+      format: 'umd',
+      name,
+      file: `dist/${name}.umd.js`,
+    },
+    {
+      format: 'es',
+      name,
+      exports: 'named',
+      file: `dist/${name}.esm.js`,
+    }, {
+      format: 'iife',
+      name,
+      extend: true,
+      exports: 'named',
+      file: `dist/${name}.min.js`,
+    }
   ],
   plugins: [
+    nodeResolve(),
     commonjs(),
     babel({
       exclude: '**/node_modules/**',
-      babelHelpers: 'runtime',
+      babelHelpers: 'bundled',
     }),
   ],
 }));
